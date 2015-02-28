9f8cfc142255c9e339877948ae18df5f18b2b594
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-02-28 13:27:38 -0300

    Removing notice regarding flipped Y axis on RenderingSettings.renderingScale

--- JelloSwift/RenderingSettings.swift
+++ JelloSwift/RenderingSettings.swift
@@ -11,8 +11,6 @@ import UIKit
 // The point size of points drawn on screen (this value is independent of the Scale vector)
 var pointSize: CGFloat = 3;
 
-// Why is the Y vector negative? The engine is a port of an XNA engine (JelloPhysics).
-// XNA is basicly 3D, so the Y vector grows up instead of down, like in a 2D screen (Say, Flash)
 var renderingScale = Vector2(25.8, 25.8);
 
 // The offset is independent of the scale, unlike Flash DisplayObject's x-y coordinates and scaleX-scaleY
