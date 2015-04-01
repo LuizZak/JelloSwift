3cf278092e59f8d0dcf88c32a621bb76f868ef6e
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-03-31 21:24:24 -0300

    . Implementing offset property for shape joint links When applying offset to shape joint links, the offset is relative to the position of the shape's body, including angle

--- JelloSwift/ShapeJointLink.swift
+++ JelloSwift/ShapeJointLink.swift
@@ -18,11 +18,17 @@ class ShapeJointLink: JointLinkType
     /// The point masses this joint is linked to
     private let _pointMasses: [PointMass];
     
+    /// The indices of this shape joint link
+    private let _indices: [Int];
+    
     /// Gets the body that this joint link is linked to
     var body: Body { return _body; }
     
     /// Gets the type of joint this joint link represents
     var linkType: LinkType { return LinkType.Shape }
+    
+    /// The offset to apply to the position of this shape joint, in body coordinates
+    var offset: Vector2 = Vector2.Zero;
     
     /// Gets the position, in world coordinates, at which this joint links with the underlying body
     var position: Vector2
@@ -35,6 +41,13 @@ class ShapeJointLink: JointLinkType
         }
         
         center /= _pointMasses.count;
+        
+        // Calculate the offset, if present
+        if(offset != Vector2.Zero)
+        {
+            // Get the angle of the points and calculate their different to the orgiinal shape angle
+            center += rotateVector(offset, _body.derivedAngle);
+        }
         
         return center;
     }
@@ -79,6 +92,7 @@ class ShapeJointLink: JointLinkType
     {
         _body = body;
         _pointMasses = pointMassIndexes.map { body.pointMasses[$0] };
+        _indices = pointMassIndexes;
     }
     
     /// Appies a given force to the subject of this joint link
@@ -90,5 +104,56 @@ class ShapeJointLink: JointLinkType
         {
             p.applyForce(force);
         }
+    }
+    
+    // TODO: Implement the function below to derive the angle of the shape's angle
+    
+    /// Returns the average angle of the vertices of this ShapeJointLink, based on the body's original shape's vertices
+    private func angle() -> CGFloat
+    {
+        // find the average angle of all of the masses.
+        var angle: CGFloat = 0;
+        
+        var originalSign: Int = 1;
+        var originalAngle: CGFloat = 0;
+        
+        for i in _indices
+        {
+            let pm = _body.pointMasses[i];
+            
+            let baseNorm = _body.baseShape.localVertices[i].normalized();
+            let curNorm  = (pm.position - _body.derivedPos).normalized();
+            
+            var dot = baseNorm =* curNorm;
+            
+            if (dot > 1.0) { dot = 1.0; }
+            if (dot < -1.0) { dot = -1.0; }
+            
+            var thisAngle = acos(dot);
+            
+            if (!vectorsAreCCW(baseNorm, curNorm)) { thisAngle = -thisAngle; }
+            
+            if (i == 0)
+            {
+                originalSign = (thisAngle >= 0.0) ? 1 : -1;
+                originalAngle = thisAngle;
+            }
+            else
+            {
+                let diff: CGFloat = (thisAngle - originalAngle);
+                let thisSign: Int = (thisAngle >= 0.0) ? 1 : -1;
+                
+                if (abs(diff) > PI && (thisSign != originalSign))
+                {
+                    thisAngle = (thisSign == -1) ? (PI + (PI + thisAngle)) : ((PI - thisAngle) - PI);
+                }
+            }
+            
+            angle += thisAngle;
+        }
+        
+        angle /= CGFloat(_pointMasses.count);
+        
+        return angle;
     }
 }
