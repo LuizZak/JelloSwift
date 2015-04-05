6641990171ede6126a91deb21f11ec1cf38b8671
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-04-05 13:31:37 -0300

    Doing further updates to the pressure component

--- JelloSwift/PressureComponent.swift
+++ JelloSwift/PressureComponent.swift
@@ -15,12 +15,10 @@ class PressureComponent: BodyComponent
     final var volume: CGFloat = 0;
     final var gasAmmount: CGFloat = 0;
     final var normalList: [Vector2] = [];
-    final var edgeLengthList: [CGFloat] = [];
     
     override func prepare(body: Body)
     {
         normalList = [Vector2](count: body.pointMasses.count, repeatedValue: Vector2());
-        edgeLengthList = [CGFloat](count: body.pointMasses.count, repeatedValue: 0);
     }
     
     override func accumulateInternalForces()
@@ -32,7 +30,6 @@ class PressureComponent: BodyComponent
         
         let c = body.pointMasses.count;
         var prev = c - 1;
-        var points = [Vector2](count: c, repeatedValue: Vector2());
         
         for (i, curEdge) in enumerate(body.edges)
         {
@@ -42,12 +39,9 @@ class PressureComponent: BodyComponent
             let edge2N = curEdge.difference;
             
             normalList[i] = (edge1N + edge2N).perpendicular().normalized();
-            edgeLengthList[i] = curEdge.length;
-            
-            points[i] = curEdge.start;
         }
         
-        volume = max(0.5, polygonArea(points));
+        volume = max(0.5, polygonArea(body.pointMasses));
         
         // now loop through, adding forces!
         let invVolume: CGFloat = 1 / volume;
@@ -55,7 +49,7 @@ class PressureComponent: BodyComponent
         for var i = 0; i < c; i++
         {
             let j = (i + 1) % c;
-            let pressureV = (invVolume * edgeLengthList[i] * (gasAmmount));
+            let pressureV = (invVolume * body.edges[i].length * (gasAmmount));
             
             body.pointMasses[i].force += normalList[i] * pressureV;
             body.pointMasses[j].force += normalList[j] * pressureV;
