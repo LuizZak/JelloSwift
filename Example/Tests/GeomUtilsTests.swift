2c37e49b6bcb83c095083d5f100de7aa3528460a
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-02-14 14:16:57 -0200

    Adding unit tests (which are broken, for now) and fixing their compilation

---
+++ Example/Tests/GeomUtilsTests.swift
@@ -0,0 +1,41 @@
+//
+//  GeomUtilsTests.swift
+//  JelloSwift
+//
+//  Created by Luiz Fernando Silva on 06/03/15.
+//  Copyright (c) 2015 Luiz Fernando Silva. All rights reserved.
+//
+
+import XCTest
+import JelloSwift
+import CoreGraphics
+import JelloSwift
+
+class GeomUtilsTests: XCTestCase
+{
+    func testLineRatio()
+    {
+        let pt1 = Vector2(0, 0)
+        let pt2 = Vector2(10, 10)
+        
+        // Edge cases
+        XCTAssertEqual(pt1, calculateVectorRatio(pt1, vec2: pt2, ratio: 0), "Failed to calculate point in line correctly")
+        XCTAssertEqual(pt2, calculateVectorRatio(pt1, vec2: pt2, ratio: 1), "Failed to calculate point in line correctly")
+        
+        // Mid-way
+        XCTAssertEqual((pt1 + pt2) / 2, calculateVectorRatio(pt1, vec2: pt2, ratio: 0.5), "Failed to calculate point in line correctly")
+    }
+    
+    func testLineRatioCentralized()
+    {
+        let pt1 = Vector2(-10, -10)
+        let pt2 = Vector2(10, 10)
+        
+        // Edge cases
+        XCTAssertEqual(pt1, calculateVectorRatio(pt1, vec2: pt2, ratio: 0), "Failed to calculate point in line correctly")
+        XCTAssertEqual(pt2, calculateVectorRatio(pt1, vec2: pt2, ratio: 1), "Failed to calculate point in line correctly")
+        
+        // Mid-way
+        XCTAssertEqual((pt1 + pt2) / 2, calculateVectorRatio(pt1, vec2: pt2, ratio: 0.5), "Failed to calculate point in line correctly")
+    }
+}
--- JelloSwift/Classes/Bitmask.swift
+++ JelloSwift/Classes/Bitmask.swift
@@ -6,21 +6,21 @@
 //  Copyright (c) 2014 Luiz Fernando Silva. All rights reserved.
 //
 
-import CoreGraphics
+import Foundation
 
 public typealias Bitmask = UInt
 
 extension Bitmask
 {
     @discardableResult
-    mutating func setBitOn(atIndex index: Int) -> UInt
+    public mutating func setBitOn(atIndex index: Int) -> UInt
     {
         self |= 1 << UInt(index > 0 ? index - 1 : 0)
         return self
     }
     
     @discardableResult
-    mutating func setBitOff(atIndex index: Int) -> UInt
+    public mutating func setBitOff(atIndex index: Int) -> UInt
     {
         self &= ~(1 << UInt(index > 0 ? index - 1 : 0))
         return self
