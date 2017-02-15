7c7786a5c775ac725a4bc834e47f02aefcc11ac8
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-02-15 09:05:43 -0200

    Plugging in unit tests

--- Example/Pods/Target Support Files/Pods-JelloSwift_Tests/Pods-JelloSwift_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_JelloSwift_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_JelloSwift_TestsVersionString[];
-
