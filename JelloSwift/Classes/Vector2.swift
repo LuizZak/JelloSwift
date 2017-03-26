995e880267bf3cc15209703ec893f3dc132aa736
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-03-26 18:28:02 -0300

    Documenting JFloat typealias

--- JelloSwift/Classes/Vector2.swift
+++ JelloSwift/Classes/Vector2.swift
@@ -16,8 +16,18 @@ public protocol VectorRepresentable {
 }
 
 #if arch(x86_64) || arch(arm64)
+/// Represents the standard floating point type used by JelloSwift.
+/// It is a double precision floating point in 64-bits platforms, and
+/// single-precision in 32-bit platforms.
+///
+/// Currently: Double
 public typealias JFloat = Double
 #else
+/// Represents the standard floating point type used by JelloSwift.
+/// It is a double precision floating point in 64-bits platforms, and
+/// single-precision in 32-bit platforms.
+///
+/// Currently: Float
 public typealias JFloat = Float
 #endif
 
