269369da75d87c792cf1cf0f9c62fdb5e35b23b4
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-07-17 09:51:54 -0300

    Comment

--- Sources/World.swift
+++ Sources/World.swift
@@ -359,7 +359,7 @@ public final class World {
             body.derivePositionAndAngle(elapsed)
             
             // Only update edge and normals pre-accumulation if the body has 
-            // components
+            // components - only components really use this information.
             if(body.componentCount > 0) {
                 body.updateEdgesAndNormals()
                 
